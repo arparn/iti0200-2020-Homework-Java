5ade8ee215dcca2d258b9102efe4d7c8ebcf9e4b
arthur <arthurp200148@gmail.com>
2020-01-31 14:00:31 +0200

    My decision.

--- EX01IdCode/src/ee/taltech/iti0200/idcode/IdCode.java
+++ EX01IdCode/src/ee/taltech/iti0200/idcode/IdCode.java
@@ -1,4 +1,6 @@
 package ee.taltech.iti0200.idcode;
+import java.util.Map;
+import java.util.HashMap;
 public class IdCode {
 
     private final String idCodeValue;
@@ -35,7 +37,41 @@ public class IdCode {
     }
 
     public String getBirthPlace() {
-        return null;
+        int identificator = 999;
+        String answer = null;
+        String idCode = getIdCodeValue();
+        String queue = idCode.substring(7, 10);
+        int queueNumber = Integer.parseInt(queue);
+        System.out.println(queueNumber);
+        if (0 > queueNumber || queueNumber > 999) {
+            return "wrong input!";
+        } else if (queueNumber == 0) {
+            return null;
+        } else {
+            Map<Integer, String> cityMap = new HashMap<Integer, String>();
+            cityMap.put(10, "Kuressaare");
+            cityMap.put(20, "Tartu");
+            cityMap.put(220, "Tallinn");
+            cityMap.put(270, "Kohtla-Järve");
+            cityMap.put(370, "Tartu");
+            cityMap.put(420, "Narva");
+            cityMap.put(470, "Pärnu");
+            cityMap.put(490, "Tallinn");
+            cityMap.put(520, "Paide");
+            cityMap.put(570, "Rakvere");
+            cityMap.put(600, "Valga");
+            cityMap.put(650, "Viljandi");
+            cityMap.put(710, "Võru");
+            cityMap.put(999, null);
+            for (Integer cityNumber : cityMap.keySet()) {
+                System.out.println(cityNumber);
+                if (queueNumber <= cityNumber && identificator >= cityNumber) {
+                    identificator = cityNumber;
+                    answer = cityMap.get(cityNumber);
+                }
+            }
+            return answer;
+        }
     }
 
     public int getFullYear() {
@@ -61,42 +97,101 @@ public class IdCode {
         String idCode = getIdCodeValue();
         char genderChar = idCode.charAt(0);
         int genderNumber = Character.getNumericValue(genderChar);
-        return 1 <= genderNumber || genderNumber <= 6;
+        return 1 <= genderNumber && genderNumber <= 6;
     }
 
     private boolean isYearNumberCorrect() {
         String idCode = getIdCodeValue();
         String year = idCode.substring(1, 3);
         int yearOfBirth = Integer.parseInt(year);
-        return 0 <= yearOfBirth || yearOfBirth <= 99;
+        return 0 <= yearOfBirth && yearOfBirth <= 99;
     }
 
     private boolean isMonthNumberCorrect() {
-        return false;
+        String idCode = getIdCodeValue();
+        String monthNumber = idCode.substring(3, 5);
+        int month = Integer.parseInt(monthNumber);
+        return 1 <= month && month <= 12;
     }
 
     private boolean isDayNumberCorrect() {
-        return false;
+        String idCode = getIdCodeValue();
+        String month = idCode.substring(3, 5);
+        int monthNumber = Integer.parseInt(month);
+        String day = idCode.substring(5, 7);
+        int dayNumber = Integer.parseInt(day);
+        int year = getFullYear();
+        return (monthNumber == 2 && isLeapYear(year) && (1 <= dayNumber && dayNumber <= 29))
+                || (monthNumber == 2 && !isLeapYear(year) && (1 <= dayNumber && dayNumber <= 28))
+                || ((1 <= monthNumber && monthNumber <= 7) && monthNumber != 2 && monthNumber % 2 == 0 && (1 <= dayNumber
+                && dayNumber <= 30))
+                || ((1 <= monthNumber && monthNumber <= 7) && monthNumber % 2 != 0 && (1 <= dayNumber && dayNumber <= 31))
+                || ((8 <= monthNumber && monthNumber <= 12) && monthNumber % 2 == 0 && (1 <= dayNumber && dayNumber <= 31))
+                || ((8 <= monthNumber && monthNumber <= 12) && monthNumber % 2 != 0 && (1 <= dayNumber && dayNumber <= 30));
     }
 
     private boolean isQueueNumberCorrect() {
-        return false;
+        String idCode = getIdCodeValue();
+        String queue = idCode.substring(7, 10);
+        int queueNumber = Integer.parseInt(queue);
+        return 0 <= queueNumber && queueNumber <= 999;
     }
 
     private boolean isControlNumberCorrect() {
-        return false;
+        String idCode = getIdCodeValue();
+        char lastChar = idCode.charAt(idCode.length() - 1);
+        int lastNumber = Character.getNumericValue(lastChar);
+        int integer = 1;
+        int integer2 = 3;
+        int sum1 = 0;
+        int sum2 = 0;
+        for (int index = 0; index < 10; index++) {
+            char character = idCode.charAt(index);
+            int number = Character.getNumericValue(character);
+            sum1 = number * integer;
+            if (integer == 9) {
+                integer = 0;
+            }
+            integer += 1;
+        }
+        int controlNumber = sum1 % 11;
+        if (controlNumber == 10) {
+            for (int index = 0; index < 11; index++) {
+                char character = idCode.charAt(index);
+                int number = Character.getNumericValue(character);
+                sum2 += number * integer2;
+                if (integer2 == 9) {
+                    integer2 = 0;
+                }
+                integer2 += 1;
+            }
+            controlNumber = sum2 % 11;
+        }
+        if (controlNumber == 10) {
+            controlNumber = 0;
+        }
+        return controlNumber == lastNumber;
     }
 
     private boolean isLeapYear(int fullYear) {
-        return false;
+        int year = getFullYear();
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else if (year % 4 == 0) {
+            return true;
+        } else {
+           return false;
+        }
     }
 
     public static void main(String[] args) {
-        IdCode validMaleIdCode = new IdCode("37605030299");
+        IdCode validMaleIdCode = new IdCode("49808276014");
         //System.out.println(validMaleIdCode.isCorrect());
         //System.out.println(validMaleIdCode.getInformation());
         //System.out.println(validMaleIdCode.getGender());
-        //System.out.println(validMaleIdCode.getBirthPlace());
+        System.out.println(validMaleIdCode.getBirthPlace());
         //System.out.println(validMaleIdCode.getFullYear());
         //System.out.println(validMaleIdCode.isGenderNumberCorrect());
         //System.out.println(validMaleIdCode.isYearNumberCorrect());
