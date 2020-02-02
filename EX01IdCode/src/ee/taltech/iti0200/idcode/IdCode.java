39a0e9614e91a190af76ab1cc1afa07bc229ea32
arthur <arthurp200148@gmail.com>
2020-02-02 14:36:16 +0200

    My decision.

--- EX01IdCode/src/ee/taltech/iti0200/idcode/IdCode.java
+++ EX01IdCode/src/ee/taltech/iti0200/idcode/IdCode.java
@@ -2,7 +2,7 @@ package ee.taltech.iti0200.idcode;
 import java.util.Map;
 import java.util.HashMap;
 public class IdCode {
-    private static int ID_CODE = 11;
+    private static int idCodeLength = 11;
 
     private final String idCodeValue;
 
@@ -26,7 +26,7 @@ public class IdCode {
                 return false;
             }
         }
-        return idCode.length() == ID_CODE && isGenderNumberCorrect() && isYearNumberCorrect()
+        return idCode.length() == idCodeLength && isGenderNumberCorrect() && isYearNumberCorrect()
                 && isMonthNumberCorrect() && isDayNumberCorrect() && isQueueNumberCorrect()
                 && isControlNumberCorrect();
     }
