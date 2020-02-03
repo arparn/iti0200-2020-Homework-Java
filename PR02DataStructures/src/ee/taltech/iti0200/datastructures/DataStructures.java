565fbc7aa5941825f9a263a3048eafd281ebf2a1
arthur <arthurp200148@gmail.com>
2020-02-03 19:17:43 +0200

    My decision.

--- PR02DataStructures/src/ee/taltech/iti0200/datastructures/DataStructures.java
+++ PR02DataStructures/src/ee/taltech/iti0200/datastructures/DataStructures.java
@@ -98,6 +98,7 @@ public class DataStructures {
      */
     public static List<String> onlyEvenWords(List<String> words) {
         ArrayList<String> answer = new ArrayList<>();
+        ArrayList<String> answer2 = new ArrayList<>();
         Map<String, Integer> mapOfWords = new HashMap<>();
         for (int i = 0; i < words.size(); i++) {
             String word = words.get(i);
@@ -116,7 +117,12 @@ public class DataStructures {
                     answer.add(key);
             }
         }
-        return answer;
+        for (String word : words) {
+            if (answer.contains(word) && !answer2.contains(word)) {
+                answer2.add(word);
+            }
+        }
+        return answer2;
     }
 
 
@@ -129,11 +135,11 @@ public class DataStructures {
         //System.out.println(rearrangeTheShelf(Arrays.asList("Raamat1-Autor1", "Raamat2-Raamatu2Autor"))); // [Raamat2, ][, Raamat1]
         //System.out.println(rearrangeTheShelf(Arrays.asList("Raamat1-Autor1", "Raamat2-Raamatu2Autor")).get(1));  // ][
         //System.out.println(rearrangeTheShelf(Arrays.asList("seiklused metsas-Saimon", "Kodumaa-Karl Gustav ", "Raamat3-Tundmatu")));  //  [Kodumaa, seiklused metsas, ][, Raamat3]
-        System.out.println(rearrangeTheShelf(Arrays.asList("Book-bob",  "raamat-Romeo", "story-teller", "teine-name", "name-nbook"))); // [name, raamat, Book, ][, story, teine]
+        //System.out.println(rearrangeTheShelf(Arrays.asList("Book-bob",  "raamat-Romeo", "story-teller", "teine-name", "name-nbook"))); // [name, raamat, Book, ][, story, teine]
 
         System.out.println(onlyEvenWords(Arrays.asList("foo", "bar", "baz", "baz", "bar", "foo"))); // [baz, bar, foo]
-        System.out.println(onlyEvenWords(Arrays.asList("a", "b", "b", "a"))); // [b, a]
-        System.out.println(onlyEvenWords(Arrays.asList("eggs", "bacon", "SPAM", "ham", "SPAM", "SPAM"))); // [SPAM]
+        //System.out.println(onlyEvenWords(Arrays.asList("a", "b", "b", "a"))); // [b, a]
+        //System.out.println(onlyEvenWords(Arrays.asList("eggs", "bacon", "SPAM", "ham", "SPAM", "SPAM"))); // [SPAM]
     }
 
 
