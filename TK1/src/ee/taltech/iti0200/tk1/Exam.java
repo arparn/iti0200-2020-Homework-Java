513a2c3d519653245cf3615bdb931494ecc2543a
arparn <arparn@ttu.ee>
2020-02-26 14:41:41 +0200

    Minu lahendus

---
+++ TK1/src/ee/taltech/iti0200/tk1/Exam.java
@@ -0,0 +1,45 @@
+package ee.taltech.iti0200.tk1;
+
+import java.util.List;
+import java.util.Map;
+public class Exam {
+
+    /**
+     * Return the sum of the numbers in the array,
+     * except ignore sections of numbers
+     * starting with a 6 and extending to the next 7
+     * (every 6 will be followed by at least one 7).
+     * Return 0 for no numbers.
+     *
+     * sum67([1, 2, 2]) => 5
+     * sum67([1, 2, 2, 6, 99, 99, 7]) => 5
+     * sum67([1, 1, 6, 7, 2]) => 4
+     */
+    public static int sum67(List<Integer> numbers) {
+        return -1;
+    }
+
+    /**
+     * Given a string, compute a new string by moving the first char to come after the next two chars, so "abc" yields "bca".
+     * Repeat this process for each subsequent group of 3 chars, so "abcdef" yields "bcaefd". Ignore any group of fewer than 3 chars at the end.
+     *
+     * oneTwo("abc") => "bca"
+     * oneTwo("tca") => "cat"
+     * oneTwo("tcagdo") => "catdog"
+     */
+    public static String oneTwo(String str) {
+        return null;
+    }
+
+    /**
+     * Modify and return the given map as follows:
+     * if exactly one of the keys "a" or "b" exists in the map (but not both), set the other to have that same value in the map.
+     *
+     * mapAXorB({"a": "aaa", "c": "cake"}) => {"a": "aaa", "b": "aaa", "c": "cake"}
+     * mapAXorB({"b": "bbb", "c": "cake"}) => {"a": "bbb", "b": "bbb", "c": "cake"}
+     * mapAXorB({"a": "aaa", "b": "bbb", "c": "cake"}) => {"a": "aaa", "b": "bbb", "c": "cake"}
+     */
+    public static Map<String, String> mapAXorB(Map<String, String> map) {
+        return null;
+    }
+}
