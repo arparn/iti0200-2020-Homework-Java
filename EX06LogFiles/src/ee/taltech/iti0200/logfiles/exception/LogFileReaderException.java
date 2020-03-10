7edb655afcf384acbd7ca31640e04b543ed00050
arthur <arthurp200148@gmail.com>
2020-03-10 20:39:12 +0200

    My decision.

--- EX06LogFiles/src/ee/taltech/iti0200/logfiles/exception/LogFileReaderException.java
+++ EX06LogFiles/src/ee/taltech/iti0200/logfiles/exception/LogFileReaderException.java
@@ -1,3 +1,19 @@
 package ee.taltech.iti0200.logfiles.exception;
 
-public class LogFileReaderException {}
+import ee.taltech.iti0200.logfiles.reader.LogFileReader;
+
+public class LogFileReaderException extends RuntimeException  {
+    String message;
+
+    public LogFileReaderException(String sonum) {
+        this.message = sonum;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- EX06LogFiles/src/ee/taltech/iti0200/logfiles/exception/LogFileWriterException.java
+++ EX06LogFiles/src/ee/taltech/iti0200/logfiles/exception/LogFileWriterException.java
@@ -1,3 +1,19 @@
 package ee.taltech.iti0200.logfiles.exception;
 
-public class LogFileWriterException {}
+import ee.taltech.iti0200.logfiles.writer.LogFileWriter;
+
+public class LogFileWriterException extends RuntimeException  {
+    String message;
+
+    public LogFileWriterException(String sonum) {
+        this.message = sonum;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
