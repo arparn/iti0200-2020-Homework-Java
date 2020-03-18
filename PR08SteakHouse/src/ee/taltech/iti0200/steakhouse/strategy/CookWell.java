f0d6e98ee8425a2c1fea925902bc230188fc5c8e
arthur <arthurp200148@gmail.com>
2020-03-18 16:31:37 +0200

    My decision.

--- PR08SteakHouse/src/ee/taltech/iti0200/steakhouse/strategy/CookWell.java
+++ PR08SteakHouse/src/ee/taltech/iti0200/steakhouse/strategy/CookWell.java
@@ -3,8 +3,11 @@ package ee.taltech.iti0200.steakhouse.strategy;
 import ee.taltech.iti0200.steakhouse.steak.SteakType;
 
 public class CookWell implements CookStrategy {
+
+    public static final int PERCENTAGE = 20;
+
     @Override
     public double calculateWeightLoss(String cookName, SteakType steakType) {
-        return (steakType.getWeight() * (double) 20) / 100;
+        return (steakType.getWeight() * (double) PERCENTAGE) / 100;
     }
 }
