f6f70ebe7aa001ec95117c9dc90cba32ce1614dd
arthur <arthurp200148@gmail.com>
2020-03-20 11:49:52 +0200

    My decision.

--- EX08BorderControl/src/ee/taltech/iti0200/bordercontrol/entity/BorderEntity.java
+++ EX08BorderControl/src/ee/taltech/iti0200/bordercontrol/entity/BorderEntity.java
@@ -2,7 +2,7 @@ package ee.taltech.iti0200.bordercontrol.entity;
 
 public abstract class BorderEntity {
 
-    abstract Long getBorderCrossingId();
+    public abstract Long getBorderCrossingId();
 
-    abstract void setBorderCrossingId(Long newBorderCrossingId);
+    public abstract void setBorderCrossingId(Long newBorderCrossingId);
 }
