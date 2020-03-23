bf6615d71910fce9f8c061ded1e69cb847174f7c
arthur <arthurp200148@gmail.com>
2020-03-23 14:39:28 +0200

    My decision.

--- PR09Greenhouse/src/ee/taltech/iti0200/greenhouse/gardener/Gardener.java
+++ PR09Greenhouse/src/ee/taltech/iti0200/greenhouse/gardener/Gardener.java
@@ -1,4 +1,20 @@
 package ee.taltech.iti0200.greenhouse.gardener;
 
+import ee.taltech.iti0200.greenhouse.plantingstrategy.PlantingStrategy;
+
+import java.util.Map;
+
 public class Gardener {
+
+    String name;
+    PlantingStrategy strategy;
+
+    public Gardener(String name, PlantingStrategy strategy) {
+        this.name = name;
+        this.strategy = strategy;
+    }
+
+    public String[][] plantPlants(int length, int width, Map<String, Integer> plants) {
+        return new String[0][];
+    }
 }
