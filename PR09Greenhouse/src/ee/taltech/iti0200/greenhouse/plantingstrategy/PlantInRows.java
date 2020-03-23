be80350947b6af69d2bbb474e1b6ba93e4e165c7
arthur <arthurp200148@gmail.com>
2020-03-23 14:41:41 +0200

    My decision.

--- PR09Greenhouse/src/ee/taltech/iti0200/greenhouse/plantingstrategy/PlantInRows.java
+++ PR09Greenhouse/src/ee/taltech/iti0200/greenhouse/plantingstrategy/PlantInRows.java
@@ -23,15 +23,15 @@ public class PlantInRows implements PlantingStrategy {
     @Override
     public String[][] plantPlants(int length, int width, Map<String, Integer> plants) {
         HashMap<String, Integer> sortedPlants = sortMap(plants);
-        String[][] greenhouse = new String[width][length];
+        String[][] greenhouse = new String[length][width];
         for (Map.Entry<String, Integer> plant : sortedPlants.entrySet()) {
             String key = plant.getKey();
             Integer value = plant.getValue();
-            for (int i = 0; i < width; i++) {
+            for (int i = 0; i < length; i++) {
                 if (value <= 0) {
                     break;
                 }
-                for (int j = 0; j < length; j++) {
+                for (int j = 0; j < width; j++) {
                     if (value <= 0) {
                         break;
                     } else if (greenhouse[i][j] == null) {
