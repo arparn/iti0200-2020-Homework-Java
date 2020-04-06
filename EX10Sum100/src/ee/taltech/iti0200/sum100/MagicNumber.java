5e886dc562065c33a225d25421533d2e22e822d0
arthur <arthurp200148@gmail.com>
2020-04-06 16:30:31 +0300

    My decision.

---
+++ EX10Sum100/src/ee/taltech/iti0200/sum100/MagicNumber.java
@@ -0,0 +1,23 @@
+package ee.taltech.iti0200.sum100;
+public interface MagicNumber {
+
+    /** Gets the number as short
+     *
+     * @return number
+     */
+    short getNumber();
+
+    /**
+     * For example if 3 cannot be subtrackted 1 + 2 - 3 is not allowed
+     *
+     * @return true, if number can have "-" in front of it
+     */
+    boolean canBeSubtracted();
+
+    /**
+     * For example if 2 cannot be placed next to other, then +-12 nor +-23 are allowed
+     *
+     * @return true, if number can be stacked with other number (Like 12)
+     */
+    boolean canBePlacedNextToOther();
+}
---
+++ EX10Sum100/src/ee/taltech/iti0200/sum100/Sum100.java
@@ -0,0 +1,9 @@
+package ee.taltech.iti0200.sum100;
+import java.util.List;
+
+public class Sum100 {
+
+    public static List<String> calcSums(List<MagicNumber> input) {
+        return null;
+    }
+}
