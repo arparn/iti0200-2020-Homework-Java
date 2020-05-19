fe49887f8dc6ac0bde4b2020a611bb2386c6a833
arthur <arthurp200148@gmail.com>
2020-05-19 10:27:27 +0300

    My decision.

---
+++ EXAM2/src/ee/taltech/iti0200/exam2/Exam.java
@@ -0,0 +1,26 @@
+package ee.taltech.iti0200.exam2;
+import java.util.List;
+
+public class Exam {
+
+    /**
+     * Write a recursive method which finds all the double and triple consecutive characters in the string.
+     * Triple consecutive characters give 2 points.
+     * Double consecutive characters give 1 point,
+     * Sums all the combinations and returns the sum.
+     * NB! Method must be recursive and not contain any for loops.  Helper functions  and are not allowed.
+     *
+     * "abc" => 0
+     * "aa" => 1
+     * "aab" => 1
+     * "aaa" => 2
+     * "aaabb" => 3
+     * "aaaaaa" => 4 (2 times triple 'a', NOT 3 times double 'a')
+     * "" => 0
+     */
+    public static int recCount(String s, int sum) {
+        return 0;
+    }
+
+}
+
--- TEST/src/Test/Test.java
+++ TEST/src/Test/Test.java
@@ -6,23 +6,5 @@ import java.util.TimerTask;
 
 public class Test {
 
-    public Integer secondsPassed = 0;
-    Timer myTimer = new Timer();
-    TimerTask task = new TimerTask() {
-        @Override
-        public void run() {
-            secondsPassed += 1;
-            System.out.println(secondsPassed);
-
-        }
-    };
-    public void timer() {
-        myTimer.schedule(task, 10000);
-    }
-
-    public static void main(String[] args) {
-        Test test = new Test();
-        test.timer();
-    }
 }
 
